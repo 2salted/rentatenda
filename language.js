a1cd11a44a311a8105df740aa6ea16750bd14354
2salted <131868085+2salted@users.noreply.github.com>
2023-06-02 20:08:06 +0000

    Add files via upload

--- language.js
+++ language.js
@@ -20,7 +20,19 @@ const li6 = document.getElementById('li6');
 const li7 = document.getElementById('li7');
 const li8 = document.getElementById('li8');
 const li9 = document.getElementById('li9');
+const li10 = document.getElementById('li10');
+const li11 = document.getElementById('li11');
+const li12 = document.getElementById('li12');
+const li13 = document.getElementById('li13');
+const thesitetext = document.getElementById('thesitetext');
+const litest = document.getElementById('litest');
+const whatweoffertext = document.getElementById('whatweoffertext')
 
+//Change the title of the page
+const newTitleEN = "RENTA TENDA - RENT A TENT KIT ON FLORES ISLAND, AZORES | tent rental services"
+const newTitleFR = "RENTA TENDA - LOUEZ UN KIT DE TENTE SUR L'ÎLE DE FLORES, AÇORES | services tent a louer"
+const newTitlePT = "RENTA TENDA - KIT ALUGUEL DE BARRACA NA ILHA DAS FLORES, AÇORES | serviços de aluguel de tendas"
+const newTitleDE = "RENTA TENDA – MIETEN SIE EIN ZELT-KIT AUF DER INSEL FLORES, AZOREN | Vermietung von Zelten"
 // Retrieve the saved language from localStorage, or default to 'en' if none is found
 const savedLang = localStorage.getItem('selectedLang') || 'en';
 
@@ -71,6 +83,19 @@ function setLanguage(lang) {
             li2.innerHTML = 'Expert customer service'
             li3.innerHTML = 'Flexible dropoff & pickup options'
             li4.innerHTML = 'Our rentals comes in a kit with all your outdoor needs!'
+            thesitetext.innerHTML = 'At The Campsites'
+            li5.innerHTML = 'Showers & Toilets'
+            li6.innerHTML = 'BBQ area (including wood)'
+            li7.innerHTML = 'Electricity for charging devices'
+            li8.innerHTML = 'Picnic tables'
+            whatweoffertext.innerHTML = 'What We Offer'
+            li9.innerHTML = 'Tents in a variety of sizes'
+            li10.innerHTML = 'Sleeping bags & Mattresses'
+            li11.innerHTML = 'Picnic sets (silverware, plates, bowls, etc)'
+            li12.innerHTML = '24/7 customer support'
+            li13.innerHTML = 'More of What We Offer'
+            litest.innerHTML = 'More of At The Campsites'
+            document.title = newTitleEN;
             break;
         case 'fr':
             //header translation
@@ -86,6 +111,19 @@ function setLanguage(lang) {
             li2.innerHTML = 'Service client expert'
             li3.innerHTML = 'Options flexibles de dépôt et de ramassage'
             li4.innerHTML = 'Nos Tent sont livrées en kit avec tous vos besoins extérieurs!'
+            thesitetext.innerHTML = 'sur les terrains de camping'
+            li5.innerHTML = 'Douches et toilettes'
+            li6.innerHTML = 'Barbecue (y compris le bois)'
+            li7.innerHTML = 'Électricité pour recharger les appareils'
+            li8.innerHTML = 'Tables de pique-nique'
+            litest.innerHTML = 'En savoir plus sur Au camping'
+            whatweoffertext.innerHTML = 'Ce que nous offrons'
+            li9.innerHTML = 'Tentes de différentes tailles'
+            li10.innerHTML = 'Sacs de couchage et Matelas'
+            li11.innerHTML = 'Ensembles de pique-nique (argenterie, assiettes, bols, etc.)'
+            li12.innerHTML = 'Assistance client 24h/24 et 7j/7'
+            li13.innerHTML = 'Plus de ce que nous offrons'
+            document.title = newTitleFR;
             break;
         case 'pt':
             //header translation
@@ -101,6 +139,19 @@ function setLanguage(lang) {
             li2.innerHTML = 'Atendimento ao cliente especializado'
             li3.innerHTML = 'Opções flexíveis de embarque e desembarque'
             li4.innerHTML = 'Nossos aluguéis vêm em um kit com todas as suas necessidades ao ar livre!'
+            thesitetext.innerHTML = 'nos acampamentos'
+            li5.innerHTML = 'Chuveiros e banheiros'
+            li6.innerHTML = 'Churrasco (incluindo madeira)'
+            li7.innerHTML = 'Eletricidade para carregar dispositivos'
+            li8.innerHTML = 'mesas de piquenique'
+            litest.innerHTML = 'Mais de At The Campsites'
+            whatweoffertext.innerHTML = 'O que nós oferecemos'
+            li9.innerHTML = 'Barracas em vários tamanhos'
+            li10.innerHTML = 'Sacos de dormir e colchões'
+            li11.innerHTML = 'Conjuntos de piquenique (talheres, pratos, tigelas, etc)'
+            li12.innerHTML = 'Suporte ao cliente 24/7'
+            li13.innerHTML = 'Mais do que oferecemos'
+            document.title = newTitlePT;
             break;
         case 'de':
             //header translation
@@ -116,6 +167,19 @@ function setLanguage(lang) {
             li2.innerHTML = 'Kompetenter Kundenservice'
             li3.innerHTML = 'Flexible Abgabe- und Abholoptionen'
             li4.innerHTML = 'Unsere Leihausrüstung wird in einem Set geliefert, das alle Ihre Outdoor-Bedürfnisse enthält!'
+            thesitetext.innerHTML = 'Auf den Campingplätzen'
+            li5.innerHTML = 'Duschen und Toiletten'
+            li6.innerHTML = 'Grill (inkl. Holz)'
+            li7.innerHTML = 'Strom für Ladegeräte'
+            li8.innerHTML = 'Picknicktische'
+            litest.innerHTML = 'Mehr von At The Campsites'
+            whatweoffertext.innerHTML = 'Was wir anbieten'
+            li9.innerHTML = 'Zelte in verschiedenen Größen'
+            li10.innerHTML = 'Schlafsäcke und Matratzen'
+            li11.innerHTML = 'Picknick-Sets (Besteck, Teller, Schüsseln usw.)'
+            li12.innerHTML = 'Kundensupport rund um die Uhr'
+            li13.innerHTML = 'Mehr von unserem Angebot'
+            document.title = newTitleDE;
             break;
         // Add cases for other languages here
         default:
